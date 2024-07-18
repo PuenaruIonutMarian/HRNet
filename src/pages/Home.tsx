e2737defb76a66a725f9cffc8bb4754a907f3a4e
Pue90 <43031720+PuenaruIonutMarian@users.noreply.github.com>
2024-07-18 23:58:56 +0200

    base config

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+
+const Home: React.FC = () => {
+    return (
+        <div className="container">
+           HR NET
+        </div>
+    );
+};
+
+export default Home;
