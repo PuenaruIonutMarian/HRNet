81c9ee24ad837b78fcf543516439423f0aa3fd22
Pue90 <43031720+PuenaruIonutMarian@users.noreply.github.com>
2024-09-28 09:33:37 +0200

    playwright settings updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,6 +24,10 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    video: 'on',
+    launchOptions: {
+      slowMo: 700,
+    },
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:5173/',
 
